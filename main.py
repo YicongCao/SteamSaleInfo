a9d67ab7812e58e5119ebd406c19b4e6b955e08e
yicongcao <yicongcao@gmail.com>
2019-02-14 17:27:21 +0800

    print utf-8 chars correctly on latin-based terminal

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+import io
+import sys
 from spider import get_sale_games
 from csv2sqlite import do_convert
 from query import do_query
@@ -7,6 +10,9 @@ BOT_HOOK = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=yourkey"
 
 
 if __name__ == '__main__':
+    # to print utf-8 chars correctly
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
     get_sale_games(5)
     do_convert()
 
